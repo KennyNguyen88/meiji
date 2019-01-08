8341bed796cdded68712855186cee10e0d85e7ab
AzureAD\NguyenDucTrung <trung.nguyen1@hubcba.com>
2019-01-08 18:57:26 +0700

    sync

--- wp-content/themes/wprig-master/dev/header.php
+++ wp-content/themes/wprig-master/dev/header.php
@@ -27,62 +27,80 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'wprig' ); ?></a>
-		<header id="masthead" class="site-header">
-			<?php if ( has_header_image() ) : ?>
-				<figure class="header-image">
-					<?php the_header_image_tag(); ?>
-				</figure>
-			<?php endif; ?>
-			<div class="site-branding">
-				<?php the_custom_logo(); ?>
-				<?php if ( is_front_page() && is_home() ) : ?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php else : ?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php endif; ?>
+    <?php echo get_template_directory_uri()."/images/temp/logo01.png"; ?>
+    <header id="header" role="banner">
+        <div id="hd_outer">
+            <h1 id="hd_logo"><a href="#"><img src="<?php echo get_template_directory_uri().'/images/temp/'?>logo01.png" width="387" height="90" alt="ほほえみクラブ ママと家族の育児応援サイト"></a></h1>
 
-				<?php $wprig_description = get_bloginfo( 'description', 'display' ); ?>
-				<?php if ( $wprig_description || is_customize_preview() ) : ?>
-					<p class="site-description"><?php echo $wprig_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
-			</div><!-- .site-branding -->
+            <div id="hd_inner">
+                <p><a href="http://www.meiji.co.jp/" target="_blank"><img src="<?php echo get_template_directory_uri().'/images/temp/'?>logo02.png" width="65" height="26" alt="Meiji"></a></p>
+                <div class="clearfix">
 
-			<nav id="site-navigation" class="main-navigation" aria-label="<?php esc_attr_e( 'Main menu', 'wprig' ); ?>"
-				<?php if ( wprig_is_amp() ) : ?>
-					[class]=" siteNavigationMenu.expanded ? 'main-navigation toggled-on' : 'main-navigation' "
-				<?php endif; ?>
-			>
-				<?php if ( wprig_is_amp() ) : ?>
-					<amp-state id="siteNavigationMenu">
-						<script type="application/json">
-							{
-								"expanded": false
-							}
-						</script>
-					</amp-state>
-				<?php endif; ?>
+                    <ul id="hd_links">
+                        <li class="last"><a href="https://line.me/R/ti/p/%40erx4059x" target="_blank">友だち追加</a></li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        <nav id="gnavi" role="navigation">
+            <ul>
+                <li id="gnavi01" class="first">
+                    <a>カテゴリ<span class="fa fa-caret-down"></span></a>
+                    <ul>
+                        <li class="first"><a href="/baby/club/category/deals/index.html">得する<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/category/eat/index.html">食べる<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/category/study/index.html">学ぶ<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/category/ask/index.html">尋ねる<span class="fa fa-caret-right"></span></a></li>
+                        <li class="last"><a href="/baby/club/category/play/index.html">動く<span class="fa fa-caret-right"></span></a></li>
+                    </ul>
+                </li>
+                <li id="gnavi02">
+                    <a href="/baby/club/tag/premama/index.html">プレママ<span class="fa fa-caret-down"></span></a>
+                    <ul>
+                        <li class="first"><a href="/baby/club/tag/pregnancy_m2/index.html">妊娠2ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m3/index.html">妊娠3ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m4/index.html">妊娠4ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m5/index.html">妊娠5ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m6/index.html">妊娠6ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m7/index.html">妊娠7ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m8/index.html">妊娠8ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/pregnancy_m9/index.html">妊娠9ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li class="last"><a href="/baby/club/tag/pregnancy_m10/index.html">妊娠10ヵ月<span class="fa fa-caret-right"></span></a></li>
+                    </ul>
+                </li>
+                <li id="gnavi03">
+                    <a href="/baby/club/tag/mama/index.html">0ヵ月～11ヵ月<span class="fa fa-caret-down"></span></a>
+                    <ul>
+                        <li class="first"><a href="/baby/club/tag/m0/index.html">生後0ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m1/index.html">生後1ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m2/index.html">生後2ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m3/index.html">生後3ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m4/index.html">生後4ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m5/index.html">生後5ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m6/index.html">生後6ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m7/index.html">生後7ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m8/index.html">生後8ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m9/index.html">生後9ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li><a href="/baby/club/tag/m10/index.html">生後10ヵ月<span class="fa fa-caret-right"></span></a></li>
+                        <li class="last"><a href="/baby/club/tag/m11/index.html">生後11ヵ月<span class="fa fa-caret-right"></span></a></li>
+                    </ul>
+                </li>
 
-				<button class="menu-toggle" aria-label="<?php esc_attr_e( 'Open menu', 'wprig' ); ?>" aria-controls="primary-menu" aria-expanded="false"
-					<?php if ( wprig_is_amp() ) : ?>
-						on="tap:AMP.setState( { siteNavigationMenu: { expanded: ! siteNavigationMenu.expanded } } )"
-						[aria-expanded]="siteNavigationMenu.expanded ? 'true' : 'false'"
-					<?php endif; ?>
-				>
-					<?php esc_html_e( 'Menu', 'wprig' ); ?>
-				</button>
-
-				<div class="primary-menu-container">
-					<?php
-
-					wp_nav_menu(
-						array(
-							'theme_location' => 'primary',
-							'menu_id'        => 'primary-menu',
-							'container'      => 'ul',
-						)
-					);
-
-					?>
-				</div>
-			</nav><!-- #site-navigation -->
-		</header><!-- #masthead -->
+                <li id="gnavi04">
+                    <a href="/baby/club/tag/age/index.html">1歳～3歳<span class="fa fa-caret-down"></span></a>
+                    <ul>
+                        <li class="item first" style="height: 57px;"><a href="/baby/club/tag/age1/index.html">1歳<span class="fa fa-caret-right"></span></a></li>
+                        <li class="item" style="height: 57px;"><a href="/baby/club/tag/age1.5/index.html">1歳1ヶ月〜1歳5ヶ月<span class="fa fa-caret-right"></span></a></li>
+                        <li class="item" style="height: 57px;"><a href="/baby/club/tag/age2/index.html">1歳半〜2歳頃<span class="fa fa-caret-right"></span></a></li>
+                        <li class="item last" style="height: 57px;"><a href="/baby/club/tag/age3/index.html">2歳〜3歳頃<span class="fa fa-caret-right"></span></a></li>
+                    </ul>
+                </li>
+                <li id="gnavi05">
+                    <a href="/baby/hatsupapa/" target="_blank">初パパ<span class="fa fa-external-link"></span></a>
+                </li>
+                <li id="gnavi06" class="last">
+                    <a href="/baby/club/category/products/index.html">明治からのご案内<span class="fa fa-caret-right"></span></a>
+                </li>
+            </ul>
+        </nav>
+    </header>
