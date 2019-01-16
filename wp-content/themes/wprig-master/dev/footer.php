717a9005497d87992eeaaaa3bf6e18a3827cbabb
AzureAD\NguyenDucTrung <trung.nguyen1@hubcba.com>
2019-01-16 18:00:22 +0700

    footer

--- wp-content/themes/wprig-master/dev/footer.php
+++ wp-content/themes/wprig-master/dev/footer.php
@@ -11,21 +11,21 @@
 
 ?>
 
-<footer id="colophon" class="site-footer">
-	<div class="site-info">
-		<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wprig' ) ); ?>">
-			<?php
-			/* translators: %s: CMS name, i.e. WordPress. */
-			printf( esc_html__( 'Proudly powered by %s', 'wprig' ), 'WordPress' );
-			?>
-		</a>
-		<span class="sep"> | </span>
-		<?php
-			/* translators: 1: Theme name, 2: Theme author. */
-			printf( esc_html__( 'Theme: %1$s by %2$s.', 'wprig' ), '<a href="' . esc_url( 'https://github.com/wprig/wprig/' ) . '">WP Rig</a>', 'the contributors' );
-		?>
-	</div><!-- .site-info -->
-</footer><!-- #colophon -->
+<div id="footer">
+    <div id="footer-inner">
+        <p class="copyright"><a href="https://www.meiji.co.jp/"><img src="<?php echo get_template_directory_uri().'/images/temp/'?>ft_logo.gif" border="0" alt="株式会社 明治 Copyright Meiji Co., Ltd. All Rights Reserved." width="180" height="25"></a></p>
+        <div class="right-section">
+            <ul class="utility-link">
+                <li class="privacy first"><a href="https://www.meiji.co.jp/privacy/">個人情報保護について</a></li>
+                <li class="rules last"><a href="https://www.meiji.co.jp/regulation/">ご利用規約</a></li>
+            </ul>
+            <ul class="company-link">
+                <li class="first"><a href="https://www.meiji.com/" target="_blank">明治ホールディングス株式会社</a></li>
+                <li class="last"><a href="https://www.meiji-seika-pharma.co.jp" target="_blank">Meiji Seika ファルマ株式会社</a></li>
+            </ul>
+        </div>
+    </div>
+</div>
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
