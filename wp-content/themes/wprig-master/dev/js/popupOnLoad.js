c95685d1f01c12445c7563ff3612acd9fd75f716
AzureAD\NguyenDucTrung <trung.nguyen1@hubcba.com>
2019-01-21 14:24:31 +0700

    init (missing)

---
+++ wp-content/themes/wprig-master/dev/js/popupOnLoad.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function(){
+    var modal = document.getElementById('myModal');
+    var btn = document.getElementById("myBtn");
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "flex";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    };
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+});
+
